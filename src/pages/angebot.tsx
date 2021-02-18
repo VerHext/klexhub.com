bbeee107fcae84d258a62550bac99d027150e3f4
Johannes Waigel <johannes@waigel.me>
2021-02-18 17:39:13 +0100

    fix: form

--- src/pages/angebot.tsx
+++ src/pages/angebot.tsx
@@ -15,20 +15,16 @@ export interface FormData {
 
 export default function Angebot() {
   const [error, setError] = useState<{ error: boolean; message: string }>()
-  const [data, setData] = useState<FormData>({
-    company_name: '',
-    email: '',
-    phone: '',
-    title_project: '',
-    budget: 'Kleines Projekt < 800€',
-    description_project: '',
-  })
+  const [data, setData] = useState<FormData>()
+  const [submitted, setSubmitted] = useState<boolean>(false)
 
   const onChangeInput = (key: string, value: string) => {
-    setData({ ...data, [key]: value })
+    console.log(key, value)
+    setData({ ...data, [key]: value.toString() })
   }
 
   const onSubmit = () => {
+    console.log(data)
     window.scrollTo({
       top: 0,
       behavior: 'smooth',
@@ -70,19 +66,12 @@ export default function Angebot() {
       error: false,
       message: 'Vielen Dank! Wir haben Ihr Angebot erhalten und melden uns schnellstmöglich.',
     })
-    setData({
-      company_name: '',
-      email: '',
-      phone: '',
-      title_project: '',
-      budget: 'Kleines Projekt < 800€',
-      description_project: '',
-    })
+    setSubmitted(true)
   }
 
   return (
     <div>
-      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 pb-12 text-klexs-500">
+      <div className="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8 pb-12 pt-12 text-klexs-500">
         <h1 className="font-bold text-2xl text-klexs-500">Angebot</h1>
         <p className=" py-6">
           Hey, lassen Sie uns mehr über Ihr Projekt wissen. Um was geht es denn? Was benötigen Sie?
@@ -93,87 +82,88 @@ export default function Angebot() {
           vorstellen.
         </p>
         <p className={`${error?.error ? 'text-red-500' : 'text-green-800'}`}>{error?.message}</p>
-        <section>
-          <div className="formItem py-3">
-            <label className="">Name Ihres Unternehmens</label>
-            <input
-              value={data?.company_name}
-              onChange={(e) => onChangeInput('company_name', e?.target?.value)}
-              type="text"
-              placeholder="StartUp GmbH"
-              className="px-3 py-3 border border-gray-300 bg-white text-sm outline-none focus:outline-none focus:shadow-outline w-full"
-            />
-          </div>
-          <div className="formItem py-3">
-            <label className="">E-Mail</label>
-            <input
-              value={data?.email}
-              onChange={(e) => onChangeInput('email', e?.target?.value)}
-              required
-              type="email"
-              placeholder="hello@startup.com"
-              className="px-3 py-3 border border-gray-300 bg-white text-sm outline-none focus:outline-none focus:shadow-outline w-full"
-            />
-          </div>
-          <div className="formItem py-3">
-            <label className="">Telefonnummer</label>
-            <input
-              value={data?.phone}
-              onChange={(e) => onChangeInput('phone', e?.target?.value)}
-              type="phone"
-              placeholder="08225 7989000"
-              className="px-3 py-3 border border-gray-300 bg-white text-sm outline-none focus:outline-none focus:shadow-outline w-full"
-            />
-          </div>
-        </section>
+        <div className="pb-32">
+          <section className={submitted ? 'hidden' : ''}>
+            <div className="formItem py-3">
+              <label className="">Name Ihres Unternehmens</label>
+              <input
+                value={data?.company_name}
+                onChange={(e) => onChangeInput('company_name', e?.target?.value)}
+                type="text"
+                placeholder="StartUp GmbH"
+                className="px-3 py-3 border border-gray-300 bg-white text-sm outline-none focus:outline-none focus:shadow-outline w-full"
+              />
+            </div>
+            <div className="formItem py-3">
+              <label className="">E-Mail</label>
+              <input
+                value={data?.email}
+                onChange={(e) => onChangeInput('email', e?.target?.value)}
+                required
+                type="email"
+                placeholder="hello@startup.com"
+                className="px-3 py-3 border border-gray-300 bg-white text-sm outline-none focus:outline-none focus:shadow-outline w-full"
+              />
+            </div>
+            <div className="formItem py-3">
+              <label className="">Telefonnummer</label>
+              <input
+                value={data?.phone}
+                onChange={(e) => onChangeInput('phone', e?.target?.value)}
+                type="phone"
+                placeholder="08225 7989000"
+                className="px-3 py-3 border border-gray-300 bg-white text-sm outline-none focus:outline-none focus:shadow-outline w-full"
+              />
+            </div>
 
-        <div className="formItem py-3">
-          <label className="">Titel Ihres Projektes</label>
-          <input
-            value={data?.title_project}
-            onChange={(e) => onChangeInput('title_project', e?.target?.value)}
-            type="text"
-            placeholder="App für unser Pizzalokal"
-            className="px-3 py-3 border border-gray-300 bg-white text-sm outline-none focus:outline-none focus:shadow-outline w-full"
-          />
-        </div>
-        <div className="formItem py-3">
-          <label id="budget">Budget</label>
-          <select
-            value={data?.budget}
-            onChange={(e) => onChangeInput('budget', e?.target?.value)}
-            className="px-3 py-3 border border-gray-300 bg-white text-sm outline-none focus:outline-none focus:shadow-outline w-full"
-          >
-            <option value="800">{'Kleines Projekt < 800€'}</option>
-            <option value="1500">{'800€ - 1.500€'}</option>
-            <option value="3000">{'1.500€ - 3.000€'}</option>
-            <option value="6000">{'3.000€ - 6.000€'}</option>
-            <option value="7000">{'6.000€ - 7.000€'}</option>
-            <option value="10000">{'7.000€ - 10.000€'}</option>
-            <option value="12000">{'10.000€ - 12.000€'}</option>
-            <option value="over-12000">{'> 12.000€'}</option>
-          </select>
-        </div>
-        <div className="formItem py-3"></div>
-        <label>Projektbeschreibung</label>
+            <div className="formItem py-3">
+              <label className="">Titel Ihres Projektes</label>
+              <input
+                value={data?.title_project}
+                onChange={(e) => onChangeInput('title_project', e?.target?.value)}
+                type="text"
+                placeholder="App für unser Pizzalokal"
+                className="px-3 py-3 border border-gray-300 bg-white text-sm outline-none focus:outline-none focus:shadow-outline w-full"
+              />
+            </div>
+            <div className="formItem py-3">
+              <label id="budget">Budget</label>
+              <select
+                value={data?.budget}
+                onChange={(e) => onChangeInput('budget', e?.target?.value)}
+                className="px-3 py-3 border border-gray-300 bg-white text-sm outline-none focus:outline-none focus:shadow-outline w-full"
+              >
+                <option value="800">{'Kleines Projekt < 800€'}</option>
+                <option value="1500">{'800€ - 1.500€'}</option>
+                <option value="3000">{'1.500€ - 3.000€'}</option>
+                <option value="6000">{'3.000€ - 6.000€'}</option>
+                <option value="7000">{'6.000€ - 7.000€'}</option>
+                <option value="10000">{'7.000€ - 10.000€'}</option>
+                <option value="12000">{'10.000€ - 12.000€'}</option>
+                <option value="over-12000">{'> 12.000€'}</option>
+              </select>
+            </div>
+            <div className="formItem py-3"></div>
+            <label>Projektbeschreibung</label>
 
-        <div className="border border-gray-300 bg-white ">
-          <SunEditor
-            defaultValue={data?.description_project}
-            height="350"
-            setDefaultStyle=" font-size: 15px;"
-            placeholder="Beschreiben Sie Ihr Projekt..."
-            onChange={(e) => onChangeInput('description_project', e)}
-          />
-        </div>
-
-        <div className="py-8">
-          <button
-            onClick={onSubmit}
-            className="mt-5 mb-8 float-right rounded-sm text-white bg-klexp-500 px-4 py-2  hover:bg-klexp-600 focus:outline-none"
-          >
-            Angebot absenden!
-          </button>
+            <div className="border border-gray-300 bg-white ">
+              <SunEditor
+                defaultValue={data?.description_project}
+                height="350"
+                setDefaultStyle=" font-size: 15px;"
+                placeholder="Beschreiben Sie Ihr Projekt..."
+                onChange={(e) => onChangeInput('description_project', e)}
+              />
+            </div>
+            <div className="py-8">
+              <button
+                onClick={onSubmit}
+                className="mt-5 mb-8 float-right rounded-sm text-white bg-klexp-500 px-4 py-2  hover:bg-klexp-600 focus:outline-none"
+              >
+                Angebot absenden!
+              </button>
+            </div>
+          </section>
         </div>
       </div>
     </div>
