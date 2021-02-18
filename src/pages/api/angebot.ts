88bf2732b4c6e217388642ca077ee6625c0b247f
Johannes Waigel <johannes@waigel.me>
2021-02-18 17:23:55 +0100

    add aws credentials

--- src/pages/api/angebot.ts
+++ src/pages/api/angebot.ts
@@ -1,5 +1,11 @@
 import axios from 'axios'
 var AWS = require('aws-sdk')
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID_MYAPP,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY_MYAPP,
+  region: process.env.AWS_REGION_MYAPP,
+  // (...)
+})
 // Set the region
 AWS.config.update({ region: 'eu-west-1' })
 
