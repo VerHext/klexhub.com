45b20593ba010c5170dd1946ba208ec798bd0ef8
Johannes Waigel <johannes@waigel.me>
2021-02-18 21:37:04 +0100

    add env for webhook_discord

--- src/pages/api/angebot.ts
+++ src/pages/api/angebot.ts
@@ -12,10 +12,9 @@ AWS.config.update({ region: 'eu-west-1' })
 // Receive the submittet form of a new angebot
 export default function submitAngebot(req, res) {
   axios
-    .post(
-      'https://discord.com/api/webhooks/810833628971532290/8hozTRL191Z1mZjWrV82GYGZUjSbegL6Eeg4YXLWZeB886XD8e1kznlG7nQ71Zhzozyz',
-      { content: '<@268368233935929344> Neues Angebot: ```' + JSON.stringify(req?.body) + '```' }
-    )
+    .post(process.env.DISCORD_WEBHOOK, {
+      content: '<@268368233935929344> Neues Angebot: ```' + JSON.stringify(req?.body) + '```',
+    })
     .catch(() => console.log('failed to send webhook discord ', JSON.stringify(req?.body)))
     .then(() => console.log('new angebot ', JSON.stringify(req?.body)))
 
@@ -81,7 +80,7 @@ const sendIncommingPassEmail = (req) => {
         /* required */
         Text: {
           Charset: 'UTF-8',
-          Data: `Vielen Dank für Ihre Interesse an unserem Unternehmen!\nWir möchten den Eingang Ihrer unverbindlichen Angebotsanfrage hiermit bestätigen.\n Wir melden uns schnellsmöglichst bei Ihnen! Bis dahin wünschen wir Ihnen alles gute!\n\n Viele Grüße\n\n KlexHub Team`,
+          Data: `Vielen Dank für Ihre Interesse an unserem Unternehmen!\n\nWir möchten den Eingang Ihrer unverbindlichen Angebotsanfrage hiermit bestätigen.\nWir melden uns schnellsmöglichst bei Ihnen! Bis dahin wünschen wir Ihnen alles gute!\n\n Viele Grüße\n\n KlexHub Team`,
         },
       },
       Subject: {
