1d4850967e5599e8435c875eac35c684caa02283
Johannes Waigel <johannes@waigel.me>
2021-03-02 20:48:22 +0100

    add google tag manager

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,6 +2,16 @@ import NextDocument, { Head, Html, Main, NextScript } from 'next/document'
 // Can import sass file this way
 // Or as an import in one of your sass files with other third party sass files
 
+const GoogleTagScript = () => {
+  return {
+    __html: `<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-P452J8C');</script>`,
+  }
+}
+
 export default class Document extends NextDocument {
   static async getInitialProps(ctx) {
     const initialProps = await NextDocument.getInitialProps(ctx)
@@ -12,6 +22,7 @@ export default class Document extends NextDocument {
     return (
       <Html lang="de" className={`text-gray-500 antialiased`}>
         <Head>
+          <div dangerouslySetInnerHTML={GoogleTagScript()} />
           <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
           <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
           <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
@@ -21,6 +32,14 @@ export default class Document extends NextDocument {
           <meta name="theme-color" content="#ffffff" />
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-P452J8C"
+              height="0"
+              width="0"
+              style={{ display: 'none', visibility: 'hidden' }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
